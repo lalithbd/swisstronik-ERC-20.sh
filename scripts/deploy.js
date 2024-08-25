16a7722367065087b82d205210d6cbeb6e626689
lalithbd <87491297+lalithbd@users.noreply.github.com>
2024-08-25 22:04:35 +0800

    Add files via upload
    ERC-20.sh

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+
+async function main() {
+  /**
+   * @dev make sure the first argument has the same name as your contract in the Hello_swtr.sol file
+   * @dev the second argument must be the message we want to set in the contract during the deployment process
+   */
+  const contract = await hre.ethers.deployContract("TestToken");
+
+  await contract.waitForDeployment();
+  const deployedContract = await contract.getAddress();
+  fs.writeFileSync("contract.txt", deployedContract);
+
+  console.log(`Swisstronik contract deployed to ${contract.target}`);
+}
+
+//DEFAULT BY HARDHAT:
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
